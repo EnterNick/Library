79ecbc148a86054aebaae38624e8e9ca3c7a80d6
EnterNick <Ravil.Mirgayazov@yandex.ru>
2024-12-28 10:59:52 +0300

    Create "book" app`s models Book and Category

--- book/models.py
+++ book/models.py
@@ -1,3 +1,20 @@
+import datetime
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+
+    desc = models.CharField(max_length=1000)
+
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    date_created = models.DateField(default=datetime.date.today)
+
+    category = models.ManyToManyField(Category)
