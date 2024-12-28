738d10d772b243965435a8c2e61439b06ebbdd80
roman <roman>
2024-12-28 13:59:58 +0300

    Add detail template

--- book_front/static/js/main.js
+++ book_front/static/js/main.js
@@ -1,16 +1,18 @@
 const wrapper = document.querySelector('.main__card-block');
+const catalogApi = 'http://127.0.0.1:8000/api/v1/catalog/'
 
-fetch('http://127.0.0.1:8000/api/v1/catalog/')
+fetch(catalogApi+window.location.search)
   .then((response) => response.json())
   .then((data) => {
     data.forEach((item) => {
       let card = document.createElement('a');
       card.classList.add('main__card');
+      card.setAttribute('href', item.id)
       card.innerHTML = `
         <h2 class="main__card-title">${item.title}</h2>
         <p class="main__card-desc">${item.desc}</p>
         <p class="main__card-date">${item.date_created}</p>
-        <a href="#" class="main__card-category">${item.category}</a>
+        <a href="?category=${item.category}" class="main__card-category">${item.category}</a>
       `
       wrapper.appendChild(card);
     });
@@ -18,3 +20,17 @@ fetch('http://127.0.0.1:8000/api/v1/catalog/')
   .catch((error) => {
     console.error('Ошибка:', error);
   });
+
+fetch(catalogApi+item.id)
+.then((response) => response.json())
+  .then((data) => {
+      let card = document.createElement('a');
+      card.classList.add('main__card');
+      card.innerHTML = `
+        <h2 class="main__card-title">${item.title}</h2>
+        <p class="main__card-desc">${item.desc}</p>
+        <p class="main__card-date">${item.date_created}</p>
+        <a href="?category=${item.category}" class="main__card-category">${item.category}</a>
+      `
+      wrapper.appendChild(card);
+});
